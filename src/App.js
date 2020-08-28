4b1a10f4ce8b264e41b5eca3b38e6b10ab87cb74
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-08-28 16:18:08 -0700

    useImperativeHandle example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const MyInput = React.forwardRef((props, ref) => {
+  const [val, setVal] = React.useState('');
+  const inputRef = React.useRef();
+
+  React.useImperativeHandle(ref, () => ({
+    blur: () => {
+      document.title = val;
+      inputRef.current.blur();
+    }
+  }));
+
   return (
-    <div className="App">
-    </div>
+    <input
+      ref={inputRef}
+      val={val}
+      onChange={e => setVal(e.target.value)}
+      {...props}
+    />
   );
-}
+});
+
+const App = () => {
+  const ref = React.useRef(null);
+  const onBlur = () => {
+    console.log(ref.current); // Only contains one property!
+    ref.current.blur();
+  };
+
+  return (
+    <>
+      <div>useImperativeHandle() allows adding functions to ref </div>
+      <br />
+      <MyInput ref={ref} onBlur={onBlur} />
+    </>
+  );
+};
 
 export default App;
